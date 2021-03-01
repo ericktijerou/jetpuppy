4afd0fe370eec8290f2e1de5c3bb62534682be5d
Erick Tijero <ericktijerou@gmail.com>
2021-03-01 11:39:04 -0500

    Remove unused imports

--- app/src/main/java/com/ericktijerou/jetpuppy/ui/onboarding/OnboardingScreen.kt
+++ app/src/main/java/com/ericktijerou/jetpuppy/ui/onboarding/OnboardingScreen.kt
@@ -27,15 +27,12 @@ import androidx.compose.animation.slideInHorizontally
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
