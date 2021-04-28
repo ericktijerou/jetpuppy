17badf3cc9d3f3341bcc681c0ba21c4b7baf7919
Erick Tijero <erick@kanto.co>
2021-04-28 07:21:59 -0500

    Increase versions

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -55,7 +55,7 @@ object Libs {
     }
 
     object Lottie {
-        private const val version = "1.0.0-alpha07-SNAPSHOT"
+        private const val version = "1.0.0-beta03-1"
         const val compose = "com.airbnb.android:lottie-compose:$version"
     }
 
@@ -95,8 +95,8 @@ object Configs {
 }
 
 object Versions {
-    internal const val appcompat = "1.2.0"
-    internal const val gradle = "7.0.0-alpha08"
+    internal const val appcompat = "1.3.0-rc01"
+    internal const val gradle = "7.0.0-alpha11"
     internal const val coroutines = "1.4.2"
     internal const val materialDesign = "1.3.0"
     internal const val coreKtx = "1.5.0-beta01"
@@ -107,8 +107,8 @@ object Versions {
     internal const val daggerHilt = "2.31-alpha"
     internal const val daggerHiltAndroid = "1.0.0-alpha03"
     internal const val composeViewModel = "1.0.0-alpha01"
-    const val kotlin = "1.4.30"
-    const val compose = "1.0.0-beta01"
+    const val kotlin = "1.4.31"
+    const val compose = "1.0.0-beta03"
     const val spotless = "5.10.0"
     const val ktlint = "0.40.0"
 }
